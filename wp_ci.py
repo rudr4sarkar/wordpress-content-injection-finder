0907356746f9051f69ca32ab7882ce7ad1b0476f
Rudra Sarkar <rudrasarkar815@gmail.com>
2017-02-07 14:59:38 +0600

    Add files via upload

---
+++ wp_ci.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import urllib3
+
+print """
+ _       ______     __________   _______           __         
+| |     / / __ \   / ____/  _/  / ____(_)___  ____/ /__  _____
+| | /| / / /_/ /  / /    / /   / /_  / / __ \/ __  / _ \/ ___/
+| |/ |/ / ____/  / /____/ /   / __/ / / / / / /_/ /  __/ /    
+|__/|__/_/       \____/___/  /_/   /_/_/ /_/\__,_/\___/_/     
+
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ +    Name        : Wordpress Content Injection Finder      +
+ +    Author      : Rudra Sarkar                            +
+ +    Date        : 04/02/2017                              +
+ +    Description : Wordpress 4.7.0 & 4.7.1 Content         +
+ +                  Injection Vulnerability -               +
+ +                  found by Sucuri Labs -                  +
+ +                  An unauthenticated user can change the  +
+ +                  contents of any blog post with a single +
+ +                  POST request.                           +
+ +                                                          +
+ +                  Thanks To Sucuri Labs :)                +
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      """
+
+# Enter your URL
+u = raw_input('Enter The URL > ')
+
+http = urllib3.PoolManager()
+
+# Param of wp json
+param = "/index.php/wp-json/wp/v2/posts/"
+
+# URL Checker ;)
+bugs = http.request('GET', u + param )
+
+# Website Data Reader
+if "guid" in bugs.data:
+		print "\n [+] Content Injection Success :D"
+		print "\n [+] POC: ", u + param
+else:
+		print "\n [!] Website Are Not Vulnerable at CI"
